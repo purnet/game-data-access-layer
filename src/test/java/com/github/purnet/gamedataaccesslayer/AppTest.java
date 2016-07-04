33b447047358eedc912fbba83d91721f8fe3e14b
purnet <teigheyyy@gmail.com>
2016-07-04 23:04:33 +1000

    Changed unit test to include adding a move

--- src/test/java/com/github/purnet/gamedataaccesslayer/AppTest.java
+++ src/test/java/com/github/purnet/gamedataaccesslayer/AppTest.java
@@ -1,31 +1,31 @@
 package com.github.purnet.gamedataaccesslayer;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import com.github.purnet.gamedataaccesslayer.entity.Game;
 import com.github.purnet.gamedataaccesslayer.entity.GameAsset;
 import com.github.purnet.gamedataaccesslayer.entity.Player;
  
 /**
- * Unit test for simple App.
+ * Unit tests for EntityResolvers.
  */
 public class AppTest extends TestCase {
  
-	public void testCreateGameBasic() {
+	private static final int testGameId = 35;
+	
+	public void testCreateGame() {
 
 		SessionManager sm = SessionManager.getInstance();
-		Session s = sm.getSession(ThreadId.get());
+		Session s = sm.createSession(ThreadId.get());
 		EntityResolvers resolver = new EntityResolvers();
 		
 		s.beginTransaction();
-		Set<GameAsset> assets = new HashSet<GameAsset>(0);
+		List<GameAsset> assets = new ArrayList<GameAsset>(0);
 		GameAsset asset = new GameAsset("d34898ec4cadc2b7b85da262e9320a9646655931","gameboard","https://assets.merknera.com/scrabble/board_standard.json");
 	    assets.add(asset);
 
@@ -35,16 +35,38 @@ public class AppTest extends TestCase {
 	    asset = new GameAsset("76d20712668ae0eb85d825b93acb71875a059a00","letters","https://assets.merknera.com/scrabble/letters_standard.json");
 	    assets.add(asset);
 	    
-	    Set<Player> players = new HashSet<Player>(0);
+	    List<Player> players = new ArrayList<Player>(0);
 	    Player player = new Player(1, "Fred");
 	    players.add(player);
 	    
 	    player = new Player(2, "Jane");
 	    players.add(player);
 
-	    Game g = resolver.createGame(35, "PENDING", assets, players);
+	    Game g = resolver.createGame(testGameId, "PENDING", assets, players);
+	    s.getTransaction().commit();
+	    
+	    s.beginTransaction();
+	    String board = "[" +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"C\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"P\", \"A\", \"S\", \"T\", \"A\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"L\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"C\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"U\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"L\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"A\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"T\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"E\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+                "[\"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\", \"\"]," +
+           "]";
+	    resolver.createMove(testGameId, board, "[\"G\", \"T\", \"C\", \"D\", \"*\", \"A\", \"A\"]");
+
 	    s.getTransaction().commit();
 	    sm.closeSession(ThreadId.get());
-		
 	}
+	
 }
